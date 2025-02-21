371f107cd89fb685a37dcd4baefac940f4b0a963
Minhluan309 <mrluan2004@gmail.com>
2025-02-21 13:11:28 +0700

    Cập nhật các tệp và thêm app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var message = 'Hello, World!';
+console.log(message);
+var number1 = '10';
+console.log(number1);
+var person = {
+    name: 'Text',
+    age: 20
+};
---
+++ app.ts
@@ -0,0 +1,14 @@
+let message: string = 'Hello, World!'
+console.log(message)
+
+let number1: string = '10'
+console.log(number1)
+
+var person: {
+    name: string,
+    age: number
+
+} = {
+    name: 'Text',
+    age: 20
+}
